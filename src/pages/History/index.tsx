741b7d8419506fdf8e7856bd4e59993a8ca981b9
daniellichotti <danielclichotti@gmail.com>
2023-12-18 15:35:11 -0300

    Contexto entre rotas - Contexto no React

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,66 +1,86 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { useContext } from 'react';
+import { HistoryContainer, HistoryList, Status } from './styles';
+import { CyclesContext } from '../../contexts/CyclesContext';
 
 export function History() {
-  return (
-    <HistoryContainer>
-      <h1>Meu Histórico</h1>
+    const { cycles } = useContext(CyclesContext);
 
-      <HistoryList>
-        <table>
-          <thead>
-            <tr>
-              <th>Tarefa</th>
-              <th>Duração</th>
-              <th>Início</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-          </tbody>
-        </table>
-      </HistoryList>
-    </HistoryContainer>
-  );
+    return (
+        <HistoryContainer>
+            <h1>Meu Histórico</h1>
+
+            <pre>{JSON.stringify(cycles, null, 2)}</pre>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    );
 }
